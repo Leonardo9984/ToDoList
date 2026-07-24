43d2a62d28106196c440fd6c4ad08179383dca15
Leonardo9984 <leosantosbiel@gmial.com>
2026-07-24 16:42:13 -0300

    function delete done

--- function.js
+++ function.js
@@ -2,19 +2,24 @@ const input = document.getElementById("addList");
 const button = document.getElementById("confirmList");
 const list = document.querySelector("ul");
 
-window.onload = () => {alert("oi")}
 button.addEventListener("click", () => {
     let valor = input.value;
     if (valor == "") {
         input.style.border = "2px solid red";
         alert("Por favor, insira um valor.");
         return;
-    }
+    } 
+
         input.style.border = "none";
         
         let li = document.createElement("li");
-        li.innerHTML = valor + "<button id='excluir'>X</button>";
+        li.innerHTML = valor + '<button type="button" class="excluir">X</button>';
+        let botaoExcluir = li.querySelector(".excluir");
+        botaoExcluir.addEventListener("click", () => {
+            li.remove();
+        });
         list.appendChild(li);
         input.value = "";
     }
 );
+
